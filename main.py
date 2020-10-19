fefbcc50047e1f24614b5b165fd412d5119ee829
GlidingRaven <rainbowkek0@gmail.com>
2020-10-19 11:24:23 +0700

    New features + tests

--- main.py
+++ main.py
@@ -3,10 +3,31 @@ import random, sys
 digits = '0123456789'
 size = 4
 
-while True:
+def filter(number):
+    if len(number) == size and \
+            all(char in digits for char in number) \
+            and len(set(number)) == size:
+        return True
+    else:
+        return False
+
+def gameLogic(guess, chosen):
+    if guess == chosen:
+        return 0, 0, 1
+    bulls = cows = 0
+    for i in range(size):
+        if guess[i] == chosen[i]:
+            bulls += 1
+        elif guess[i] in chosen:
+            cows += 1
+    return bulls, cows, 0
+
+# главная функция. Один вызов - одна игровая сессия
+def gameTry():
     chosen = ''.join(random.sample(digits, size))
     print ('Я выбрал число, состоящее из цифр от 0 до 9, расположенных в случайном порядке.\nПредположи, какие %s цифры я выбрал и введи их. Я отвечу, сколько из них угаданы на неверных позициях (коровы), а какие угаданы вплоть до позиции (быки).\nУдачи!' % (size))
-    # print(chosen)
+    print(chosen)
+
     guesses = 0
     while True:
         guesses += 1
@@ -14,22 +35,25 @@ while True:
             # спросить пользователя
             guess = input('\nПопытка [%i]: ' % guesses).strip()
             # проверка на неправильный ввод
-            if len(guess) == size and \
-                    all(char in digits for char in guess) \
-                    and len(set(guess)) == size:
+            if filter(guess):
                 break
             print ("Попробуй ещё раз. Введи %i неповторяющихся цифр от 0 до 9" % size)
-        if guess == chosen:
-            print ('\nМоё почтенье, угадал с ', guesses, ' раза.\nЕсли желаете сыграть ещё раз, введите 1. Для выхода введите 0')
+
+        bulls, cows, win = gameLogic(guess, chosen)
+        if win == 1:
+            print('\nМоё почтенье, угадал с ', guesses,
+                  ' раза.\nЕсли желаете сыграть ещё раз, введите 1. Для выхода введите 0')
             again = input('Ещё раз?').strip()
             if again != '1':
                 sys.exit()
             else:
                 break
-        bulls = cows = 0
-        for i in range(size):
-            if guess[i] == chosen[i]:
-                bulls += 1
-            elif guess[i] in chosen:
-                cows += 1
         print ('  %i Быка\n  %i Коровы' % (bulls, cows))
+# вызов главной функции. Число её вызовов = числу попыток. В нашем случае - бесконечность
+
+def main():
+    while True:
+        gameTry()
+
+if __name__ == "__main__":
+    main()
--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,23 @@
-def inc(x):
-    return x + 1
+import main as main
 
-def test_answer():
-    assert inc(4) == 5
+def test_filter():
+	assert main.filter('') == False
+	assert main.filter('123') == False
+	assert main.filter('12345') == False
+	assert main.filter('12a4') == False
+	assert main.filter('1244') == False
+
+	assert main.filter('1234') == True
+
+def test_logic():
+	bulls, cows, win = main.gameLogic('1234', '1234')
+	assert [bulls, cows, win] == [0, 0, 1]
+
+	bulls, cows, win = main.gameLogic('5678', '1234')
+	assert [bulls, cows, win] == [0, 0, 0]
+
+	bulls, cows, win = main.gameLogic('1239', '1234')
+	assert [bulls, cows, win] == [3, 0, 0]
+
+	bulls, cows, win = main.gameLogic('1324', '1234')
+	assert [bulls, cows, win] == [2, 2, 0]
